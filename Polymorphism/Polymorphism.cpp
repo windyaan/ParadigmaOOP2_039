c1f713f49d4c28477f1dee6b6e800f817a1a396a
windyaan <windyanurmaretha@gmail.com>
2023-05-31 19:28:16 +0700

    Fungsi utama untuk menjalankan program

--- Polymorphism/Polymorphism.cpp
+++ Polymorphism/Polymorphism.cpp
@@ -31,3 +31,18 @@ public:
 
 
 };
+
+int main() {
+	seseorang* orang;
+	joko a;
+	lia b;
+
+	orang = &a;
+	orang->pesan();
+	orang = &b;
+	orang->pesan();
+	//a.seseorang::pesan();	//memanggil method yang berada di class orang tua
+	//b.seseorang::pesan();
+
+	return 0;
+}
